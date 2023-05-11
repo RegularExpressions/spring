240e99c1f4073aa24444add11747d835b42c0b07
zhaoyao <jingchenxi555>
2023-05-11 10:53:59 +0800

    111

---
+++ spring-study-ioc/src/main/java/cn/study/MainStart.java
@@ -0,0 +1,18 @@
+package cn.study;
+
+import cn.study.beans.UserServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("cn.study")
+public class MainStart {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new AnnotationConfigApplicationContext(MainStart.class);
+		UserServiceImpl bean = context.getBean(UserServiceImpl.class);
+		bean.sayHi();
+	}
+}
